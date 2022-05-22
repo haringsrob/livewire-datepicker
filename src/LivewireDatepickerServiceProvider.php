1fd04c4782854d577581cdb70d5a300bec1ce80d
Harings Rob <haringsrob@gmail.com>
2022-05-22 11:20:42 +0200

    Cleanup service provider.

--- src/LivewireDatepickerServiceProvider.php
+++ src/LivewireDatepickerServiceProvider.php
@@ -3,8 +3,6 @@
 namespace Haringsrob\LivewireDatepicker;
 
 use Haringsrob\LivewireDatepicker\Http\Livewire\DatePickerComponent;
-use Haringsrob\LivewireDatepicker\Views\Calendar;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
 
@@ -17,13 +15,9 @@ class LivewireDatepickerServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'haringsrob');
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'livewire-datepicker');
 
         Livewire::component('datepicker', DatePickerComponent::class);
-        Blade::component('livewire-datepicker::calendar', Calendar::class);
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
@@ -39,11 +33,6 @@ class LivewireDatepickerServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/livewire-datepicker.php', 'livewire-datepicker');
-
-        // Register the service the package provides.
-        $this->app->singleton('livewire-datepicker', function ($app) {
-            return new LivewireDatepicker;
-        });
     }
 
     /**
@@ -69,21 +58,8 @@ class LivewireDatepickerServiceProvider extends ServiceProvider
         ], 'livewire-datepicker.config');
 
         // Publishing the views.
-        /*$this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/haringsrob'),
-        ], 'livewire-datepicker.views');*/
-
-        // Publishing assets.
-        /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/haringsrob'),
-        ], 'livewire-datepicker.views');*/
-
-        // Publishing the translation files.
-        /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/haringsrob'),
-        ], 'livewire-datepicker.views');*/
-
-        // Registering package commands.
-        // $this->commands([]);
+        $this->publishes([
+            __DIR__ . '/../resources/views' => base_path('resources/views/vendor/haringsrob'),
+        ], 'livewire-datepicker.views');
     }
 }
